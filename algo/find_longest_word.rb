d245959677b26295d54c2d4d83e06cd764df1bff
Brandon Baah <brandonbaah@Brandons-MBP.home>
2016-07-17 21:06:51 -0400

    Longest word will be found

--- algo/find_longest_word.rb
+++ algo/find_longest_word.rb
@@ -1,8 +1,14 @@
 # Complete the method called find_longest_word, which will accept a string as a parameter (usually a sentence), and return another string that will be the longest word in that sentence.
-
+class Wordbomb
 def find_longest_word(sentence)
-
-end
+  words = []
+  words << sentence
+  largest_word_so_far = words[0]
+  words.each do |word|
+  if word.length > largest_word_so_far
+    largest_word_so_far = word
+    end
+ end
 
 # Driver code - don't touch anything below this line.
 puts "TESTING find_longest_word..."
@@ -18,4 +24,6 @@ if result == "longest"
   puts "PASS!"
 else
   puts "F"
+end
+end
 end
